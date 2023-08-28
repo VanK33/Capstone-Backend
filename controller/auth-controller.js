b15c8881b729a7b3ae924e6bd55a6155e9357b93
Jeff Ma <jeff.ma713@gmail.com>
2023-08-28 14:39:47 -0700

    complete server side auth, with auth-middleware, and added corrosponding routes

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
+const verifyToken = require("../middleware/auth-middleware");
 
 const signUp = async (req, res) => {
   try {
@@ -36,26 +37,60 @@ const signUp = async (req, res) => {
   }
 };
 
-const logIn = async (req, res) => {
-  const user = await knex("contributors").where("username", username).first();
+const logIn = async (_req, res) => {
+  try {
+    const user = await knex("contributors").where("username", username).first();
 
+    if (!user) {
+      return res.status(401).json({
+        message: "invalid credentails",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        message: "invalid credentails",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: "invalid credentails",
+    });
+  }
+};
+
+const getProfile = async (req, res) => {
+  const { id, username } = req.user;
+
+  const user = await knex("contributors").where("id", id).first();
   if (!user) {
-    return res.status(401).json({
-      message: "invalid credentails",
-    });
+    return res.status(404).json({ message: "User not found" });
   }
 
-  const passwordMatch = await bcrypt.compare(password, user.password);
+  const userProfile = {
+    id: user.id,
+    username: user.username,
+    contributor_name: user.contributor_name,
+    // TODO: if future add more content related to the user
+    // remeber to link them here.
+  };
 
-  if (!passwordMatch) {
-    return res.status(401).json({
-      message: "invalid credentails",
-    });
-  }
-
-  const token = jwt.sign({ id: user.id, username: user.username }, "");
+  return res.status(200).json(userProfile);
 };
 
 module.exports = {
   signUp,
+  logIn,
+  getProfile: [verifyToken, getProfile],
 };
